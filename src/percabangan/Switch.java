459419690a9ccee017fbbf57f18bc9d5c46b370b
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-27 15:23:31 +0800

    Percabangan Switch dengan Char

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -20,5 +20,28 @@ public class Switch {
                 System.out.println("mahluk halus");
                 break;
         }
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs     : \"Terima kasih Pak\"");
+                System.out.println("Dosen   : \"Selamat ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs     : \"kenapa Ulun kada dapat A, pak?\"");
+                System.out.println("Dosen   : \"!@#$%\"");
+                break;
+            case 'C':
+                System.out.println("Mhs     : \"Ulun turun pul pak ai, tugas pul jua\"");
+                System.out.println("Dosen   : \"Tapi bisa lah menjawab ujian?\"");
+                System.out.println("Mhs     : \"Hihihi...\"");
+                break;
+            default:
+                System.out.println("Mhs     : \"Ulun turun pul pak ai, tugas pul jua\"");
+                System.out.println("Dosen   : \"Bujur jua kah?\"");
+                System.out.println("Dosen   : \"*memeriksa berkas\"");
+                System.out.println("Dosen   : \"*menceleng\"");
+                System.out.println("Mhs     : \"kabur\"");
+                break;
+        }
     }
 }
