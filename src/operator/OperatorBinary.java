5e6e0f92f0de9e516e45c27e3b8a8a4f0ac5162e
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-27 14:51:38 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Siti Nur Azizah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
