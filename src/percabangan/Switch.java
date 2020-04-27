76deb7dde4ba12aebbeeb0fea711393654de0e79
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-27 15:22:03 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan = 4;
+        switch (jalanJalan) {
+            case 1:
+                System.out.println("Urang Lapas");
+                break;
+            case 2:
+                System.out.println("Urang acayan");
+                break;
+            case 3:
+                System.out.println("Ubat Nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            case 5:
+                System.out.println("mahluk halus");
+                break;
+        }
+    }
+}
