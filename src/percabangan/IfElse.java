284dff2d8ee5b7d5c4034756a2fc9682bad9437e
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-27 15:18:29 +0800

    If Condition1 bernilai True, If Condition2 bernilai False

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,8 +2,8 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 45000;
-        double uangDiDompet = 62000;
+        double totalBelanja = 62000;
+        double uangDiDompet = 45000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uangnya Kurang, Kurangi Jajan Anda");
