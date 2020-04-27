d144ee5bfff2c0f22fd500a5e129c10898ad099a
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-27 15:06:23 +0800

    Contoh If dengan Condition False

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 62000;
-        double uangDiDompet = 45000;
+        double totalBelanja = 45000;
+        double uangDiDompet = 62000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uangnya Kurang, Kurangi Jajan Anda");
         }else{
-            System.out.println("Uangnya cukup, Selamat Menikmati");
+            System.out.println("Uangnya cukup, Berelaan lah sanak");
         }
     }
 }
